d6dddb813b33a33adf788ba709b1b461dc888144
Rakibul Hasan <santukhan02@gmail.com>
2019-02-15 21:04:10 +0600

    Testing tournament results

--- wp-content/themes/alchemists-child/copa-includes/tournament-results.php
+++ wp-content/themes/alchemists-child/copa-includes/tournament-results.php
@@ -47,11 +47,11 @@ $output .= '</tr></thead><tbody>';
 $the_tournament = get_post($sp_tournament);
 
 foreach($results as $result){
-    var_dump($result);
     
     $output .= '<tr>';
 
     $table = new SP_Event((int)$result->meta_value);
+    var_dump($result->meta_value);
 
     $data = $table->results();
 
